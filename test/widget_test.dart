3037e558d4f93e2201e19d1f5cfa571bca9ad768
Alexandre SOARES <72209227+AS-37@users.noreply.github.com>
2023-05-31 00:49:52 +0200

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -15,20 +15,6 @@ import 'package:geolocator/geolocator.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
 
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-
     /*TESTS API ET BASIC AUTH
     print('TESTS API ET BASIC AUTH');
     String username = '9a596bfe-ceb4-42e4-a64f-c03be692c8f8';
