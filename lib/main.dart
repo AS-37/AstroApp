b89d3c6c84a6ed688bc8023b2e12e7e15c8cfd77
Alexandre SOARES <72209227+AS-37@users.noreply.github.com>
2023-05-30 19:49:28 +0200

    Working on tests

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      title: 'AstroApp',
+      theme: ThemeData(primarySwatch: Colors.indigo),
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
